e2135b959b8905667d9dfcb918067e4e20bd2038
aryanpamwani-offical <aryanpamwani@outlook.com>
2026-03-25 00:01:56 +0530

    Pushing code

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
          
           <p className="flex flex-col gap-2 w-full whitespace-pre-line font-bold text-3xl">
           <span className='italic text-yellow-400 mb-3'
-          >"Panache 2026"</span>
+          >{`"Panache 2026"`}</span>
       {/* H3 errror dega hydation ka isliye okay sun isko panache ko Italics me kadocenerme chaiye bhai 5 minute ma ghar se connect karta hu bss nikl rha hu  okay*/}
            The annual techno-management fest of the College of Pharmacy, celebrating the vibrant spirit, talent, and creativity of students. Blending academic excellence with cultural expression, the fest brings together a dynamic lineup of competitions, performances, and innovative activities. It serves as a platform for students to showcase their skills, foster collaboration, and create unforgettable experiences filled with energy, enthusiasm, and inspiration.
 
